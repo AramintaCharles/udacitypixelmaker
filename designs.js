2fcae5d11adb7fdc8df05aaa7f5448013391c470
AramintaCharles <40742516+AramintaCharles@users.noreply.github.com>
2018-07-01 22:39:51 +0200

    Update designs.js

--- designs.js
+++ designs.js
@@ -2,24 +2,22 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
-
 $(document).ready(function () {
   $("#sizePicker").submit(function (event) {
     event.preventDefault();
 
     var row;
-    var column;
+    var col;
     var color;
 
-    row = $("#inputHeight").val();   // height
-    column = $("#inputWidth").val();   // width
-    makeGrid(row, column);
+    row = $("#inputHeight").val(); 
+    col = $("#inputWidth").val();   
+    makeGrid(row, col);
   });
 });
+
 function makeGrid(height, width) {
   $("tr").remove();
-
-  
   for (var x = 1; x <= height; x++) {
     $("#pixelCanvas").append("<tr></tr>");
 
