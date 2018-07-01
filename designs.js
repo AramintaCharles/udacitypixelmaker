9eeab2aff4e8386709c1102f2f0ad4a729e012ca
AramintaCharles <40742516+AramintaCharles@users.noreply.github.com>
2018-07-01 22:36:14 +0200

    Update designs.js

--- designs.js
+++ designs.js
@@ -3,8 +3,15 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+function makeGrid(height, width) {
+  $("tr").remove();
 
-// Your code goes here!
+  // Your code goes here!
+  for (var x = 1; x <= height; x++) {
+    $("#pixelCanvas").append("<tr></tr>");
 
+    for (var y = 1; y <= width; y++) {
+      $('tr').filter(':last').append('<td></td>');
+    }
+  }
 }
